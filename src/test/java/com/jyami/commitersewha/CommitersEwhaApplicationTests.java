c5c4798aac918d1232d6c12f16864451f65ae67b
mjung1798 <mor2222@naver.com>
2020-07-27 16:25:31 +0900

    Feat: project setting

---
+++ src/test/java/com/jyami/commitersewha/CommitersEwhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyami.commitersewha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitersEwhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
